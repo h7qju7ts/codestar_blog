e5a486f8e243f75ca5cce36c2e2b38b2fbec06c1
Adrian <h7qju7ts@students.codeinstitute.net>
2025-10-07 19:42:04 +0100

    fix typo in security key and add local ip adr to allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -24,13 +24,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.enviton.get("SECRET_KEY")
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = [
     '.herokuapp.com',
+     '127.0.0.1',
+    
 ]
 
 
