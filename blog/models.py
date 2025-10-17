02b732aa86a610067f0c99f77e9c9657780c86a5
Adrian <h7qju7ts@students.codeinstitute.net>
2025-10-17 13:58:44 +0100

    add Meta class Tto blogzPost, create urls.py , add include to path , update urlpatterns

--- blog/models.py
+++ blog/models.py
@@ -13,11 +13,24 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt = models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+         return f"Contains :  {self.title} | written by {self.author}"    
 
 class Comment(models.Model):
         post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments")
         author = models.ForeignKey(User, on_delete=models.CASCADE)
         body = models.TextField()
         approved = models.BooleanField(default=False)
-        created_on = DateTimeField(auto_now_add=True)
+        created_on = models.DateTimeField(auto_now_add=True)
+        
+
+        class Meta:
+             ordering = ["created_on"]
+
+        def __str__(self):
+             return f"Comment {self.body} by {self.author}"     
     
