7ff2c7d15441803526e4ed2121d73100bab44f7e
Adrian <h7qju7ts@students.codeinstitute.net>
2025-10-26 13:14:19 +0000

    update blog\views.py

--- blog/views.py
+++ blog/views.py
@@ -26,6 +26,7 @@ def post_detail(request, slug):
     post = get_object_or_404(queryset, slug=slug)
     comments = post.comments.all().order_by("-created_on")
     comment_count = post.comments.filter(approved=True).count()
+    comment_form = CommentForm()
 
     return render(
         request,"blog/post_detail.html",
