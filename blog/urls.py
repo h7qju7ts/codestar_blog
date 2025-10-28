abe11deb6e7f66a3c2bf93272a18dc5f2df40e62
Adrian <h7qju7ts@students.codeinstitute.net>
2025-10-28 13:26:01 +0000

    add comment_delete to URL patterns

--- blog/urls.py
+++ blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
          name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>',
          views.comment_edit, name='comment_edit'),
+    path('<slug:slug>/delete_comment/<int:comment_id>',
+         views.comment_delete, name='comment_delete'),     
 ]
